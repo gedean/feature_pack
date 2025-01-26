df59b10829e776587d9c55a69ba700af4d97026d
Gedean Dias <gedean.dias@gmail.com>
2025-01-26 15:05:32 -0300

    Add build and install script for feature_pack gem

---
+++ build_and_install.sh
@@ -0,0 +1,6 @@
+rm *.gem
+gem build feature_pack.gemspec
+# Lista todos os arquivos .gem, ordena alfabeticamente e pega o último
+latest_gem=$(ls -1 *.gem | sort | tail -n 1)
+# Instala o arquivo .gem mais recente
+gem install "$latest_gem"
